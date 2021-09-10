4479ea006f9c83927c1615c5d8b5afc24a478009
whale124 <hyeonje1147@naver.com>
2021-09-10 22:17:32 +0900

    v1.1

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #2021/09/10
 
+# Putty에서 색표현이 제대로 안될 때
+#https://superuser.com/questions/335471/using-colour-schemes-with-vim-and-putty
+
 echo "INSTALL"
 
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get update && sudo apt-get upgrade
 
 echo "\n\nclang&ctags"
 sudo apt-get install clang && sudo apt-get install ctags
@@ -19,7 +22,7 @@ echo "\n\nVimrc"
 mv .vim* $HOME/
 
 echo "\n\nVim Plugin"
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "\n\nAutoremove"
 sudo apt-get autoremove
